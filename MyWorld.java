a23be864b61b9a1b98df8fde1503f43e6d00b7dc
MZhang-9 <m2volleyballa@gmail.com>
2024-09-13 12:41:16 -0400

    Done the bomb

--- MyWorld.java
+++ MyWorld.java
@@ -15,7 +15,9 @@ public class MyWorld extends World
      */
     public MyWorld()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        
+        bomb bomb = new bomb();
+        addObject(bomb, 600, 100);
     }
 }
---
+++ bomb.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class bomb extends Actor
+{
+    int moveSpeed = -5;
+    public void act()
+    {
+        move(moveSpeed);
+        if(getX() <= 0) {
+            resetBomb();
+            moveSpeed -= 0.25;
+        }
+//        if(isTouching(Hero.class)) {
+//            getWorld().removeObject(this);
+//        }
+    }
+    public void resetBomb() {
+        int num = Greenfoot.getRandomNumber(2);
+        if(num == 0) {
+            setLocation(600, 100);
+        }else {
+            setLocation(600, 300);
+        }
+        
+    }
+}
